b574ba1b759686e16a8d0559cbc4f5c2a6b3befb
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-05-23 21:25:49 +0400

    Use reference for completion + use string[] for xml-response

--- embeditorRPC/src/org/jetbrains/plugins/embeditor/EmbeditorRequestHandler.java
+++ embeditorRPC/src/org/jetbrains/plugins/embeditor/EmbeditorRequestHandler.java
@@ -1,8 +1,5 @@
 package org.jetbrains.plugins.embeditor;
 
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionResult;
-import com.intellij.codeInsight.completion.CompletionService;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
@@ -17,13 +14,13 @@ import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-import com.intellij.util.Consumer;
+import com.intellij.psi.PsiReference;
+import com.intellij.util.ArrayUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.Collections;
 
 import static com.google.common.collect.Sets.newHashSet;
 
@@ -32,87 +29,87 @@ import static com.google.common.collect.Sets.newHashSet;
  * Date: 5/23/13
  */
 public class EmbeditorRequestHandler {
-    private static final Logger LOG = Logger.getInstance(EmbeditorRequestHandler.class);
+  private static final Logger LOG = Logger.getInstance(EmbeditorRequestHandler.class);
 
-    // XML-RPC interface method - keep the signature intact
-    @NotNull
-    @SuppressWarnings("UnusedDeclaration")
-    public Collection<String> getCompletionVariants(@NotNull String path, int offset) {
-        LOG.debug("getCompletionVariants(" + path + ", " + offset);
-        PsiFile targetFile = findTargetFile(path);
-        if (targetFile != null) {
-            final Collection<String> variants = newHashSet();
-            final CompletionParameters parameters = null;
-            CompletionService.getCompletionService().performCompletion(parameters, new Consumer<CompletionResult>() {
-                @Override
-                public void consume(CompletionResult completionResult) {
-                    variants.add(completionResult.getLookupElement().toString());
-                }
-            });
-            return variants;
+  // XML-RPC interface method - keep the signature intact
+  @NotNull
+  @SuppressWarnings("UnusedDeclaration")
+  public String[] getCompletionVariants(@NotNull String path, int offset) {
+    LOG.debug("getCompletionVariants(" + path + ", " + offset);
+    PsiFile targetFile = findTargetFile(path);
+    if (targetFile != null) {
+      final PsiReference referenceAt = targetFile.findReferenceAt(offset);
+      if (referenceAt != null) {
+        final Collection<String> variants = newHashSet();
+        final Object[] referenceAtVariants = referenceAt.getVariants();
+        for (Object referenceAtVariant : referenceAtVariants) {
+          variants.add(referenceAt.toString());
         }
-        return Collections.emptyList();
+        return variants.toArray(new String[variants.size()]);
+      }
+    }
+    return ArrayUtil.EMPTY_STRING_ARRAY;
+  }
+
+  @Nullable
+  private PsiFile findTargetFile(@NotNull String path) {
+    Pair<VirtualFile, Project> data = new File(path).isAbsolute() ? findByAbsolutePath(path) : findByRelativePath(path);
+    return data != null ? PsiManager.getInstance(data.second).findFile(data.first) : null;
+  }
+
+  @Nullable
+  private static Pair<VirtualFile, Project> findByAbsolutePath(@NotNull String path) {
+    File file = new File(FileUtil.toSystemDependentName(path));
+    if (file.exists()) {
+      VirtualFile vFile = findVirtualFile(file);
+      if (vFile != null) {
+        Project project = ProjectLocator.getInstance().guessProjectForFile(vFile);
+        if (project != null) {
+          return Pair.create(vFile, project);
+        }
+      }
     }
 
-    @Nullable
-    private PsiFile findTargetFile(@NotNull String path) {
-        Pair<VirtualFile, Project> data = new File(path).isAbsolute() ? findByAbsolutePath(path) : findByRelativePath(path);
-        return data != null ? PsiManager.getInstance(data.second).findFile(data.first) : null;
+    return null;
+  }
+
+  @Nullable
+  private static Pair<VirtualFile, Project> findByRelativePath(@NotNull String path) {
+    Project[] projects = ProjectManager.getInstance().getOpenProjects();
+    String localPath = FileUtil.toSystemDependentName(path);
+
+    for (Project project : projects) {
+      File file = new File(project.getBasePath(), localPath);
+      if (file.exists()) {
+        VirtualFile vFile = findVirtualFile(file);
+        return vFile != null ? Pair.create(vFile, project) : null;
+      }
     }
 
-    @Nullable
-    private static Pair<VirtualFile, Project> findByAbsolutePath(@NotNull String path) {
-        File file = new File(FileUtil.toSystemDependentName(path));
-        if (file.exists()) {
+    for (Project project : projects) {
+      for (VcsRoot vcsRoot : ProjectLevelVcsManager.getInstance(project).getAllVcsRoots()) {
+        VirtualFile root = vcsRoot.getPath();
+        if (root != null) {
+          File file = new File(FileUtil.toSystemDependentName(root.getPath()), localPath);
+          if (file.exists()) {
             VirtualFile vFile = findVirtualFile(file);
-            if (vFile != null) {
-                Project project = ProjectLocator.getInstance().guessProjectForFile(vFile);
-                if (project != null) {
-                    return Pair.create(vFile, project);
-                }
-            }
+            return vFile != null ? Pair.create(vFile, project) : null;
+          }
         }
-
-        return null;
+      }
     }
 
-    @Nullable
-    private static Pair<VirtualFile, Project> findByRelativePath(@NotNull String path) {
-        Project[] projects = ProjectManager.getInstance().getOpenProjects();
-        String localPath = FileUtil.toSystemDependentName(path);
+    return null;
+  }
 
-        for (Project project : projects) {
-            File file = new File(project.getBasePath(), localPath);
-            if (file.exists()) {
-                VirtualFile vFile = findVirtualFile(file);
-                return vFile != null ? Pair.create(vFile, project) : null;
-            }
-        }
-
-        for (Project project : projects) {
-            for (VcsRoot vcsRoot : ProjectLevelVcsManager.getInstance(project).getAllVcsRoots()) {
-                VirtualFile root = vcsRoot.getPath();
-                if (root != null) {
-                    File file = new File(FileUtil.toSystemDependentName(root.getPath()), localPath);
-                    if (file.exists()) {
-                        VirtualFile vFile = findVirtualFile(file);
-                        return vFile != null ? Pair.create(vFile, project) : null;
-                    }
-                }
-            }
-        }
-
-        return null;
-    }
-
-    @Nullable
-    private static VirtualFile findVirtualFile(@NotNull final File file) {
-        return ApplicationManager.getApplication().runWriteAction(new Computable<VirtualFile>() {
-            @Nullable
-            @Override
-            public VirtualFile compute() {
-                return LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file);
-            }
-        });
-    }
+  @Nullable
+  private static VirtualFile findVirtualFile(@NotNull final File file) {
+    return ApplicationManager.getApplication().runWriteAction(new Computable<VirtualFile>() {
+      @Nullable
+      @Override
+      public VirtualFile compute() {
+        return LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file);
+      }
+    });
+  }
 }

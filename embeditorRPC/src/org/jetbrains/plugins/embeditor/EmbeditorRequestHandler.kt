0e300820235b3977c5e13952ad0692a5fc5070c1
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-05-24 19:20:39 +0400

    Rename and extract getCompletionPrefixLength method

--- embeditorRPC/src/org/jetbrains/plugins/embeditor/EmbeditorRequestHandler.kt
+++ embeditorRPC/src/org/jetbrains/plugins/embeditor/EmbeditorRequestHandler.kt
@@ -4,24 +4,18 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
-import com.intellij.util.ArrayUtil
 
 val LOG = Logger.getInstance(javaClass<EmbeditorRequestHandler>())
 
 public class EmbeditorRequestHandler {
-  public fun getStartCompletionOffset(path: String, fileContent: String, line: Int, column: Int): Int {
+  public fun getCompletionPrefixLength(path: String, fileContent: String, line: Int, column: Int): Int {
     LOG?.debug("getStartCompletionOffset(${path}:${line}:${column}")
     var result = 0
     EmbeditorUtil.performCompletion(path, fileContent, line, column, object: EmbeditorUtil.CompletionCallback {
       override fun completionFinished(parameters: CompletionParameters,
                                       items: Array<out LookupElement>,
                                       document: Document) {
-        val range = parameters.getPosition().getTextRange()
-        if (range != null) {
-          val offset = range.getStartOffset()
-          val lineNumber = document.getLineNumber(offset)
-          result = offset - document.getLineStartOffset(lineNumber)
-        }
+        result = EmbeditorUtil.getCompletionPrefixLength(parameters, document)
       }
     })
     return result
--- vim-intellij-client/plugin/idea.vim
+++ vim-intellij-client/plugin/idea.vim
@@ -23,7 +23,7 @@ lastlines = vim.current.buffer[row+1:]
 filecontent = '\n'.join(firstlines) + '\n' + currentline + '\n' + '\n'.join(lastlines)
 
 if vim.eval("a:findstart") == '1':
-    result = server.embeditor.getStartCompletionOffset(filepath, filecontent, row, col)
+    result = server.embeditor.getCompletionPrefixLength(filepath, filecontent, row, col)
 else:
     result = server.embeditor.getCompletionVariants(filepath, filecontent, row, col)
 

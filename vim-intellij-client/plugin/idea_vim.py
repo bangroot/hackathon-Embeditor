843649e07164209ff2d2a8f370619783f3f95825
Eliseo Martínez <eliseomarmol@gmail.com>
2014-02-21 00:51:26 +0100

    Refactor: Cleanup python.
    - Improve variable names.
    - PEP8 style guide compliance (fix long lines).

--- vim-intellij-client/plugin/idea_vim.py
+++ vim-intellij-client/plugin/idea_vim.py
@@ -1,8 +1,7 @@
 import vim
 import xmlrpclib
-import traceback  # for exception output
 
-IDEA_RPC_HOST = "http://localhost"
+IDEA_RPC_HOST = 'http://localhost'
 IDEA_RPC_PORT = 63341
 
 
@@ -12,39 +11,45 @@ def current_file_path():
 
 def resolve():
     file_path = current_file_path()
-    (row, col) = get_caret_position()
+    row, col = get_caret_position()
     file_content = '\n'.join(vim.current.buffer)
-    result = server().resolve(file_path, file_content, row, col)
+    results = server().resolve(file_path, file_content, row, col)
 
     # todo: replace with for and add popup
-    if len(result) > 0:
-        resolveResult = result[0]
-        targetFilePath, targetLine, targetColumn = extract_values_from_resolve_outcome(resolveResult)
-        if file_path != targetFilePath:
+    if len(results) > 0:
+        result = results[0]
+        path, line, column = resolve_result_values(result)
+        if file_path != path:
             # todo: add file existing checking
-            vim.command(":e %s" % targetFilePath)
-        set_caret_pos(targetLine, targetColumn)
+            vim.command(":e %s" % path)
+        set_caret_position(line, column)
 
 
-def extract_values_from_resolve_outcome(resolve_outcome):
-    return resolve_outcome["path"], resolve_outcome["line"] + 1, resolve_outcome["column"]
+def resolve_result_values(result):
+    return result["path"], result["line"] + 1, result["column"]
 
 
 def complete():
     find_start = int(vim.eval('a:findstart'))
     base_string = vim.eval('a:base')
 
-    (row, col) = get_caret_position()
     file_path = current_file_path()
+    row, col = get_caret_position()
     first_lines = vim.current.buffer[:row]
-    current_line = vim.current.buffer[row][:col] + base_string + vim.current.buffer[row][col:]
+    current_line = (vim.current.buffer[row][:col] +
+                    base_string +
+                    vim.current.buffer[row][col:])
     last_lines = vim.current.buffer[row + 1:]
-    file_content = '\n'.join(['\n'.join(first_lines), current_line, '\n'.join(last_lines)])
+    file_content = '\n'.join(['\n'.join(first_lines),
+                              current_line,
+                              '\n'.join(last_lines)])
 
     if find_start:
-        result = server().getCompletionStartOffsetInLine(file_path, file_content, row, col)
+        result = server().getCompletionStartOffsetInLine(
+            file_path, file_content, row, col)
     else:
-        result = server().getCompletionVariants(file_path, file_content, row, col)
+        result = server().getCompletionVariants(
+            file_path, file_content, row, col)
 
     vim.command('return %s' % result)
 
@@ -55,12 +60,12 @@ def server():
 
 def get_caret_position():
     # todo: col should consider tabwidth
-    (row, col) = vim.current.window.cursor
+    row, col = vim.current.window.cursor
     row = row - 1
     return row, col
 
 
-def set_caret_pos(line, column):
+def set_caret_position(line, column):
     if len(vim.current.buffer) >= line:
         vim.current.window.cursor = (line, 1)
         if len(vim.current.buffer[line - 1]) >= column:
